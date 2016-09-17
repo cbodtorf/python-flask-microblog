8953db7a352d118aa2ddb827062f542a9438fd86
Cbodtorf <cbodtorf@gmail.com>
2016-09-17 11:51:27 -0400

    added a base template, and some logic in our templates to dynamically add content from our python code. (for right now is hardcoded)

--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
+from flask import render_template
 from app import blg
 
 
 @blg.route('/')
 @blg.route('/index')
 def index():
-    return "Hello, World"
+    user = {'nickname': 'Kelb'}  # fake user... or is he?
+    posts = [  # fake array of posts
+            {
+                'author': {'nickname': 'John'},
+                'body': 'Beautiful day in Portland!'
+                },
+            {
+                'author': {'nickname': 'Susan'},
+                'body': 'The Avengers movie was so cool!'
+                }
+            ]
+    return render_template(
+        'index.html', title=user['nickname'], user=user, posts=posts)
